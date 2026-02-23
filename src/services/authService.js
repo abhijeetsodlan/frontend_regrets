5a5587d2ac44d347339c3a6031287dd5ac462c84
Gaurav Shokhanda <gauravshokanda2@gmail.com>
2026-02-23 20:35:46 +0530

    login

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,7 +2,15 @@
 import { apiClient, buildAuthConfig } from "./apiClient";
 import { csrfCookieUrl, googleAuthUrl } from "./config";
 
-export const getGoogleLoginUrl = () => googleAuthUrl;
+export const getGoogleLoginUrl = () => {
+  if (typeof window === "undefined" || !window.location?.origin) {
+    return googleAuthUrl;
+  }
+
+  const url = new URL(googleAuthUrl);
+  url.searchParams.set("redirect", window.location.origin);
+  return url.toString();
+};
 
 export const requestCsrfCookie = async () => {
   await axios.get(csrfCookieUrl, { withCredentials: true });
